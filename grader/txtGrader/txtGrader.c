8821c47912ad816739e3a6c9ddd4a6ec75ccf5bd
escovec <escovec@wwu.edu>
2020-11-15 18:10:19 -0800

    Logic working for question stats. May need format tweaks

--- grader/txtGrader/txtGrader.c
+++ grader/txtGrader/txtGrader.c
@@ -4,23 +4,29 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include <ctype.h>
 
 #define MAXLINE 100
 
+int gradeQuestion(char* ans, char* keyAns, int testStats[][4]);
+
 int main(int argc, char **argv) {
-	if(argc != 3){
+	if(argc != 4){
 		fprintf(stderr, "Bad input. Usage:\n./txtGrader path_to_tests path_to_keys\n");
 		exit(EXIT_FAILURE);
 	}
+
+	int numQuestions = atoi(argv[3]);
+	int testStats[numQuestions][4];
+	memset(testStats, 0, numQuestions*4*sizeof(int));
+
 	char* keyNames[4] = {"testKeyA.txt", "testKeyB.txt","testKeyC.txt","testKeyD.txt"};
 	char* versions[4] = {"A\n", "B\n", "C\n", "D\n"};
 
 	char testLoc[1024];
 	char keyLoc[1024];
 
-	FILE *testFile;
-	FILE *keyFile;
-	FILE *gradeFile;
+	FILE *testFile, *keyFile, *gradeFile, *statsFile;
 
 	char ans[MAXLINE];
 	char keyAns[MAXLINE];
@@ -28,6 +34,7 @@ int main(int argc, char **argv) {
 	int testIterator = 1;
 
 	gradeFile = fopen("grades.csv", "w+");
+	statsFile = fopen("stats.csv", "w+");
 
 	while(1){
 		char currentTest[1024];
@@ -64,29 +71,45 @@ int main(int argc, char **argv) {
 
 		int numCorrect = 0;
 		int currentQ = 1;
-		char multipleFlag[400];
-		bzero(multipleFlag, strlen(multipleFlag));
 		while(fgets(keyAns, MAXLINE, keyFile) != NULL){
 			fgets(ans, MAXLINE, testFile);
-			if(strlen(ans) > 2){
-				char str[10];
-				bzero(str, sizeof(str));
-				snprintf(str, sizeof(str), "%d ", currentQ);
-				strcat(multipleFlag, str);
-			}else if(strcmp(ans, keyAns)==0){
-				numCorrect++;
-			}
+			numCorrect += gradeQuestion(ans, keyAns, testStats);
 			currentQ++;
 		}
-		if(strlen(multipleFlag)>0){
-			fprintf(gradeFile, "%s,%d, Bad Input For: %s\n", wNum, numCorrect, multipleFlag);
-		}else{
-			fprintf(gradeFile, "%s,%d\n", wNum, numCorrect);
-		}
+
+		fprintf(gradeFile, "%s,%d\n", wNum, numCorrect);
+
 		fclose(testFile);
 		fclose(keyFile);
 	}
 	fclose(gradeFile);
 
+	fprintf(statsFile, "Question Number, A,B,C,D\n");
+	for(int i=0; i<numQuestions; i++){
+		fprintf(statsFile, "Question %d, %d,%d,%d,%d\n", i+1, testStats[i][0], testStats[i][1], testStats[i][2], testStats[i][3]);
+	}
+	fclose(statsFile);
+}
 
+int gradeQuestion(char* ans, char* keyAns, int testStats[][4]){
+	char keyVal[strlen(keyAns)+1];
+	strncpy(keyVal, keyAns, sizeof(keyVal));
+
+	int wasCorrect = 0;
+	char* realAns = strtok(keyVal, " ");
+	char* mapToQ = strtok(NULL, ":");
+	char* mapToAns = strtok(NULL, "\n");
+
+	if(strlen(ans) == 1){
+		if(strcmp(ans, realAns) == 0){
+			wasCorrect = 1;
+		}
+	}
+	int mapToQuestion = atoi(mapToQ);
+	int qIndx = ans[0] - 65;
+	int standardizedAns = mapToAns[qIndx] - '0';
+
+	testStats[mapToQuestion-1][standardizedAns-1] += 1;
+
+	return wasCorrect;
 }
