27c30956258a4c8b2d6f9e418ccbcd421dc3908c
Cameron Wallace <wallac21@wwu.edu>
2021-03-03 21:15:31 -0800

    Added comments, updated Tex Template File

--- gui/ScantronGUIAlpha/createTest.c
+++ gui/ScantronGUIAlpha/createTest.c
@@ -169,6 +169,8 @@ void createExam(FILE *key,                 // File pointer for exam key
   FILE *fp, *texH, *texM, *texE;
   char c;
   char buf[LEN];
+  int i;
+  
   snprintf(buf, sizeof(buf), "./%s/Book/%s/exam_%c.tex", courseInfo[0], courseInfo[1], form);
   fp = fopen(buf, "w");
   /* Static .TeX portion of all exams */
@@ -176,7 +178,7 @@ void createExam(FILE *key,                 // File pointer for exam key
   texM = fopen("./texTemplate/mid", "r");
   texE = fopen("./texTemplate/end", "r");
 
-  /* Header */
+  /* Write tex header template to new exam. */
   c = fgetc(texH);
   while (c != EOF) {
     fputc(c, fp);
@@ -184,11 +186,12 @@ void createExam(FILE *key,                 // File pointer for exam key
   }
   fclose(texH);
   
-  /* Specific Test/Year */
+  /* Write specific Test/Year string to new exam. */
   snprintf(buf, sizeof(buf), "%s, %s - Test %d (%d pts), Form %C\n",
 	   courseName, courseYear, testNum, testScore ,form);
   fputs(buf, fp);
-  
+
+  /* Write tex mid section template to new exam. */
   c = fgetc(texM);
   while (c != EOF) {
     fputc(c, fp);
@@ -199,13 +202,19 @@ void createExam(FILE *key,                 // File pointer for exam key
   /* Questions */
   printf("form: %c\n", form);
   
-  /* Declare start of form in key file */
+  /* Declare start of form and write to key file */
   snprintf(buf, sizeof(buf), "Form_%c\n", form);
   fputs(buf, key);
+
+  /* Calculate how the questions will be split - used for scrambling */
   int capacity = questionC / formC;
-  //int overflow = questionC % formC;
+  /* Initialize array to store long answers positions. */
   int longAnswers[questionC];
-  int i;
+  
+  /* Loop through all questions in two chunks. All questions are split into n sections
+     where n is the number of forms. These two loops shift the question order by 1/n
+     per form. When parseQuestion returns < 0, that question is marked to be added to 
+     the end of the exam */
   for (i = (currentForm * capacity); i < questionC; i++) {
     if (parseQuestion(fp, key, fileList, questionC, i) < 0) {
       longAnswers[i] = 1;
@@ -217,7 +226,7 @@ void createExam(FILE *key,                 // File pointer for exam key
     }
   }
   
-  /* Put the long answer questions at the end */
+  /* Put the long answer questions at the end of the other questions. */
   for (i = 0; i < questionC; i++) {
     if (longAnswers[i] == 1) {
       snprintf(buf, sizeof(buf), "./questions/%s", fileList[i]);
@@ -227,7 +236,7 @@ void createExam(FILE *key,                 // File pointer for exam key
     }
   }
   
-  /* End */
+  /* Write the end tex template to the new exam file. */
   c = fgetc(texE);
   while (c != EOF) {
     fputc(c, fp);
@@ -241,7 +250,7 @@ void createExam(FILE *key,                 // File pointer for exam key
  * function: get contents from question file and write them to exam tex file,
  *           record correct answers to key file 
  */
-int parseQuestion(FILE *fp,                 // File pointer for exam
+int parseQuestion(FILE *fp,                  // File pointer for exam
 		   FILE *key,                // File pointer for exam key
 		   char **fileList,          // List containing question file paths
 		   int questionC,            // Number of total questions
@@ -250,24 +259,27 @@ int parseQuestion(FILE *fp,                 // File pointer for exam
   FILE *cQ;
   int answers = 0;
 
+  /**/
   snprintf(buf, sizeof(buf), "./questions/%s", fileList[questionNum]);
   printf("question %d = %s\n", questionNum, buf);
 
-  /* current question */
+  /* cQ is the file of the current question to be written. */
   cQ = fopen(buf, "r");
 
-  /* Count the number of answers in file */
+  /* Count the number of answers in the question. */
   while (fgets(buf, LEN, cQ) != NULL) {
+    /* Return -1 if the question is long answer style. */
     if (buf[0] == 'L') {
       return -1;
     }
+    /* If the character is an A (answer) or X (correct answer) increment */
     if ((buf[0] == 'A' || buf[0] == 'X') && buf[1] == '-') {
       answers++;
     }
   }
   rewind(cQ);
 
-  /* Write to new file */
+  /* Write to new exam file */
   writeQuestionToFile(fp, cQ);
 
   /* Key Buffer Sizing */
@@ -284,11 +296,13 @@ int parseQuestion(FILE *fp,                 // File pointer for exam
 
 /* @author Cameron Wallace
  * function: Helper for parseQuestions
+ * Key file lines follow the format: a 1:12345
+ * - [correct answer] [question num on form A]:[order of answers]
  */
 int setupKeyBuffer (char *keyBuf,            // Buffer used to generate exam key
 		    int questionNum) {       // Number of questions on the exam
   int digits = 0;
-  /* Determine length of buffer */
+  /* Determine length needed for the buffer used to write the exam key */
   if (0 <= questionNum+1 && questionNum+1 < 10) digits = 1;
   if (10 <= questionNum+1 && questionNum+1 < 100) digits = 2;
 
@@ -316,25 +330,34 @@ void writeQuestionToFile(FILE *fp,            // File pointer for the exam
   int inQ = 0, inV = 0; /* In question or In verbatim */
   rewind(cQ);
   while (fgets(buf, LEN, cQ) != NULL) {
-    if ((buf[0] == 'Q' || buf[0] == 'L') && buf[1] == '-') { /*  Question */
+    /*  Question */
+    if ((buf[0] == 'Q' || buf[0] == 'L') && buf[1] == '-') {
       inQ = 1;
       snprintf(buf2, sizeof(buf2), "\\item (3 points) %s", buf + 2);
       fputs(buf2, fp);
-    } else if (buf[0] == 'V' && buf[1] == '-' && !inV) { /* Start Verbatim */
+    }
+    /* Start Verbatim */
+    else if (buf[0] == 'V' && buf[1] == '-' && !inV) {
       inQ = 0; inV = 1;
       snprintf(buf2, sizeof(buf2), "\\begin{verbatim}\n    %s", buf + 2);
       fputs(buf2, fp);
-    } else if ((buf[0] == 'A' || buf[0] == 'X') && buf[1] == '-') { /* End Verbatim */
+    }
+    /* End Verbatim */
+    else if ((buf[0] == 'A' || buf[0] == 'X') && buf[1] == '-') { 
       if (inV == 1) {
 	snprintf(buf2, sizeof(buf2), "\\end{verbatim}\n");
 	fputs(buf2, fp);
       }
       inV = 0;
       continue;
-    } else {
-      if (inQ) { /* Line is still part of the question */
+    }
+    else {
+      /* Line is still part of the question */
+      if (inQ) {
 	fputs(buf, fp);
-      } else if (inV) { /* Line is still part of the verbatim */
+      }
+      /* Line is still part of the verbatim */
+      else if (inV) { 
 	snprintf(buf2, sizeof(buf2), "    %s", buf);
 	fputs(buf2, fp);
       }
@@ -353,10 +376,11 @@ void writeAnswersToFile(FILE *fp,                  // File pointer for exam
 			FILE *key,                 // File pointer for exam key
 			FILE *cQ,                  // File pointer for current question
 			char *keyBuf,              // Buffer used to generate exam key 
-			int ans,                   //
-			int d,                     //
-			bool randomize) {          //
+			int ans,                   // Number of answers in question file
+			int digits,                // Number of digits in key buffer
+			bool randomize) {          // Whether answer choice order is random
   int i;
+  int dCount;
   char buf[LEN];
   char buf2[LEN];
 
@@ -379,18 +403,26 @@ void writeAnswersToFile(FILE *fp,                  // File pointer for exam
   char correct;
   fputs("  \\begin{enumerate}\n", fp);
   for (i = 0; i < ans; i++) {
-    int dCount = i + d + 3;
-    if (answerList[i][0] == 'A') {   /* Non-Answer */
+    dCount = i + d + 3;
+    /* Non-Answer */
+    if (answerList[i][0] == 'A') {   
       snprintf(buf, sizeof(buf), "  \\item %s", answerList[i] + 2);
       fputs(buf, fp);
       keyBuf[dCount] = (i+1) + 48;
-    } else if (answerList[i][0] == 'X') {  /* Answer */
+    }
+    /* Answer */
+    else if (answerList[i][0] == 'X') {  
       switch(i) {
-      case 0: correct = 'a'; break;
-      case 1: correct = 'b'; break;
-      case 2: correct = 'c'; break;
-      case 3: correct = 'd'; break;
-      case 4: correct = 'e'; break;
+      case 0:
+	correct = 'a'; break;
+      case 1:
+	correct = 'b'; break;
+      case 2:
+	correct = 'c'; break;
+      case 3:
+	correct = 'd'; break;
+      case 4:
+	correct = 'e'; break;
       }
       char tmp[LEN];
       snprintf(buf, sizeof(buf), "  \\item %s", answerList[i] + 2);
